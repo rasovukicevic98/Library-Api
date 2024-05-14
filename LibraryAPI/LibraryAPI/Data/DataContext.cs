25cc9135d6d7b7b0782bd7d3bc6b135cc81446cb
rasovukicevic98 <rasovukicevic98@gmail.com>
2024-05-14 09:51:54 +0000

    Merged PR 144: Registered DI
    Registered DI

---
+++ LibraryAPI/LibraryAPI/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using LibraryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace LibraryAPI.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options):base(options) 
+        {
+            
+        }
+
+        public DbSet<User> Users {  get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+        }
+
+    }
+}
---
+++ LibraryAPI/LibraryAPI/Repository/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using LibraryAPI.Models;
+using LibraryAPI.Services;
+
+namespace LibraryAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        public IEnumerable<User> GetUsers()
+        {
+            var users = new List<User>();
+            users.Add(new User {
+                Name = "RASO",
+                Email="raso",
+                Id = "1",
+                Password="raso"
+            });
+
+            return users;
+        }
+    }
+}
